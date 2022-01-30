f45a015d31c5a4512347676ec56ea3f33c7af3b7
semihagnyldz <semihagn@gmail.com>
2022-01-30 11:56:30 -0500

    see

--- src/test/java/com/cybertek/pages/gitPractice.java
+++ src/test/java/com/cybertek/pages/gitPractice.java
@@ -3,4 +3,10 @@ package com.cybertek.pages;
 public class gitPractice {
     //hello semiha this is your first practice in git
     //more commits
+    //more commits
+    //isim ekleme
+    //isim ekleme again
+    //isim ekleme en son
+
+
 }
